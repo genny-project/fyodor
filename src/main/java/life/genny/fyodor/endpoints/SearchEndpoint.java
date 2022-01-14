a1038106d4d14fd78fe921141d99e76b6bd00235
Jasper Robison <jasper.robison22@gmail.com>
2022-01-14 17:36:35 +1100

    remove log

--- src/main/java/life/genny/fyodor/endpoints/SearchEndpoint.java
+++ src/main/java/life/genny/fyodor/endpoints/SearchEndpoint.java
@@ -94,7 +94,6 @@ public class SearchEndpoint {
 		if (attributes != null) {
 
 			String json = jsonb.toJson(attributes);
-			log.info("sending attrs = " + json);
 			return Response.ok().entity(json).build();
 		}
 		return Response.status(Response.Status.NOT_FOUND).build();
